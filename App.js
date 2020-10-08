d117d07143875165b8cb6b3692e79a9fdd2c31dc
Andrea Corrado <andcor02@MacBook-Pro.device>
2020-10-08 02:17:04 +0100

    commented code

--- App.js
+++ App.js
@@ -4,34 +4,31 @@ const TeenyURL = require('./db/TeenyURL')
 const app = express()
 require('dotenv').config()
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/TeenyURL', {
-  useNewUrlParser: true, useUnifiedTopology: true
+TeenyURL.collection.deleteMany({}) //delete collection on start of app
+
+app.set('view engine', 'ejs') //set view engine to ejs
+app.use(express.urlencoded({ extended: false })) //needed for url formatting
+
+app.get('/', async (req, res) => { //async operation
+  const teenyget = await TeenyURL.find() //load collection
+  res.render('index', { teenyget: teenyget }) //render collection for index
 })
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-
-app.get('/', async (req, res) => {
-  const teenyget = await TeenyURL.find()
-  res.render('index', { teenyget: teenyget })
-})
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); //route public files access
 
 app.post('/teenyget', async (req, res) => {
-  await TeenyURL.create({ full: req.body.fullUrl })
-
-  res.redirect('/')
+  await TeenyURL.create({ full: req.body.fullUrl }) //create db entry from form input of long URL
+  res.redirect('/') //return
 })
 
-app.get('/:teenyURL', async (req, res) => {
-  const teenyURL = await TeenyURL.findOne({ short: req.params.teenyURL })
-  if (teenyURL == null) return res.sendStatus(404)
+app.get('/:teenyURL', async (req, res) => { 
+  const teenyURL = await TeenyURL.findOne({ short: req.params.teenyURL }) 
+  if (teenyURL == null) return res.sendStatus(404) //if collection returns no entries return 404
 
-  teenyURL.clicks++
-  teenyURL.save()
+  teenyURL.clicks++ //increment db clicks if found
+  teenyURL.save() //save to db
 
-  res.redirect(teenyURL.full)
+  res.redirect(teenyURL.full) //render and redirect to full link
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000); //listen on port 3000.
--- db/TeenyURL.js
+++ db/TeenyURL.js
@@ -1,22 +1,24 @@
 const mongoose = require('mongoose')
-const TeenyID = require('shortid')
+const TeenyID = require('shortid') //A really useful package that generates a short ID
 require('dotenv').config()
 
+//Create simple schema containg full url, shorter url, and number of clicks on shorter url
 const shortUrlSchema = new mongoose.Schema({
   full: {
     type: String,
-    required: true
+    required: true //required to be filled when adding to db
   },
   short: {
     type: String,
-    required: true,
-    default: TeenyID.generate
+    required: true, //required to be filled when adding to db
+    default: TeenyID.generate //Default value calls shortid package places into 'short' colummn
   },
+  
   clicks: {
     type: Number,
-    required: true,
+    required: true, //required to be filled when adding to db
     default: 0
   }
 })
 
-module.exports = mongoose.model('TeenyURL', shortUrlSchema)
+module.exports = mongoose.model('TeenyURL', shortUrlSchema) //export our schema for app.js
